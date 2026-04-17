53db8d7df00dd310d35988a38b4fb0efd43ef9cd
alae-ai <alaemoussalaachiri7@gmail.com>
2026-04-17 23:11:33 +0100

    FINAL BARE MINIMUM

---
+++ src/main/java/com/planning/microplanning/web/controller/RootController.java
@@ -0,0 +1,14 @@
+package com.planning.microplanning.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String root() {
+        return "Planning Service is running";
+    }
+}
+
